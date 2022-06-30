671710e8c4e20a0de8df3e353e7d1a773c338755
RJD02 <dulangeraviraj@gmail.com>
2022-06-30 17:33:10 +0530

    Update

---
+++ cp-essentials/Graph-DSA/Cycle-Detection/cycle_detection_undirected_bfs.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+bool solve(int n, vector<vector<int>> edges) {
+	vector<vector<int>> adjList(n + 1);
+
+	for (auto i : edges) {
+		adjList[i[0]].push_back(i[1]);
+		adjList[i[1]].push_back(i[0]);
+	}
+
+	// for(auto i: adjList) {
+	// 	for(auto j: i) {
+	// 		cout << j << " ";
+	// 	}
+	// 	cout << endl;
+	// }
+
+	auto bfs = [&] (int source = 1) {
+		queue<int> q;
+		bool visited[n + 1];
+		int parent[n + 1];
+
+		q.push(source);
+		visited[source] = 1;
+		parent[source] = -1;
+		while (!q.empty()) {
+			int node = q.front(); q.pop();
+			for (auto nbr : adjList[node]) {
+				cout << nbr << " ";
+				if (!visited[nbr]) {
+					q.push(nbr);
+					visited[nbr] = true;
+					parent[nbr] = node;
+				} else if (nbr != parent[node])
+					return true;
+			}
+		}
+		return false;
+	};
+
+
+	return bfs();
+}
+
+int main() {
+	int n = 5;
+	vector<vector<int>> edges = {{1, 2}, {1, 3}, {2, 3}, {1, 4}, {4, 5}};
+	cout << solve(n, edges) << endl;
+	return 0;
+}
