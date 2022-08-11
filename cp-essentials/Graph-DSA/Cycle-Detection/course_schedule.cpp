749f87770840131a3cecd1d477766e6e7875fe75
RJD02 <dulangeraviraj@gmail.com>
2022-08-11 10:11:02 +0530

    Course schedule

---
+++ cp-essentials/Graph-DSA/Cycle-Detection/course_schedule.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+bool canFinish(vector<vector<int>> prerequisites, int n) {
+	vector<vector<int>> adjList(n);
+	vector<int> inDegree(n);
+	for (auto i : prerequisites) {
+		adjList[i[1]].push_back(i[0]);
+		inDegree[i[0]]++;
+	}
+	// Topological sort
+	stack<int> st;
+
+	for (int i = 0; i < n; i++) {
+		if (inDegree[i] == 0)
+			st.push(i);
+	}
+
+	int count = 0;
+
+	while (!st.empty()) {
+		int node = st.top(); st.pop();
+		count++;
+		for (auto i : adjList[node]) {
+			inDegree[i]--;
+			if (inDegree[i] == 0)
+				st.push(i);
+		}
+	}
+	// count < n => unconnected
+	// count > n => cyclic
+	return count == n;
+}
