fdc7ced8d08de9c460da9e883b33ff61c5ad2340
RJD02 <dulangeraviraj@gmail.com>
2023-04-13 08:44:47 +0530

    Add java module

---
+++ coding-ninjas/java/input.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Input {
+public static void main(String[] args)
+{
+  int a=5,b=4;
+  System.out.println(a*=b);//line 1
+  System.out.println(a=(a==b));//line 2
+  System.out.println(a==b);//line 3
+}
+}
---
+++ coding-ninjas/java/some.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	string s= "123";
+	for(auto i: s)
+		cout << i - '0' << " ";
+	return 0;
+}
